40795b889e37b390539c9b794b7ea65e10cefc0e
Daniel <dmolcap@gmail.com>
2021-10-25 20:51:25 +0100

    Add more contract calls

--- src/App.js
+++ src/App.js
@@ -14,20 +14,28 @@ class App extends Component {
   // this is ES2016 equivalent to the constructor function above
   state = {
     manager: "",
+    players: [],
+    balance: "",
   };
 
   async componentDidMount() {
     //we don't need to specify the "from: address" inside the call() because working with metamask the default account is provided
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div className="App">
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}. 
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
       </div>
     );
   }
